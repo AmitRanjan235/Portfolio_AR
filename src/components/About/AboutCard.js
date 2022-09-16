7a70a12bbeb35e43726605f1b5f7b4e29794dbe4
AmitRanjan235 <99864721+AmitRanjan235@users.noreply.github.com>
2022-09-17 00:39:32 +0530

    intial commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Amit Ranjan </span>
+            from <span className="purple"> Bihar, India.</span>
+            <br />I am a Data science enthusiast pursuing B-Tech in CSE in RGIPT JAIS.
+            
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Games
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Writting Tech Blogs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Amit</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
