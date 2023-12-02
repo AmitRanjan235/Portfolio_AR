4bf6375d83d217bfcb1d7eeef571b958b367017d
AmitRanjan235 <99864721+AmitRanjan235@users.noreply.github.com>
2023-12-03 02:08:22 +0530

    navbar twitter link changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://mobile.twitter.com/amitranjan1139"
+                href=""
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
